75a0132b2863c7f263a6dadb32126d2f9511990b
Brandon Matran <b_matran@comcast.net>
2023-03-16 00:33:23 -0700

    Endpoint to get all players and specific player

--- server.py
+++ server.py
@@ -1,15 +1,26 @@
 from flask import Flask, jsonify
-from nba_api.stats.static import players
 import os
+import json
 
 app = Flask(__name__)
+
 
 origins = [
     "http://localhost:8000",
     os.environ.get("PUBLIC_URL", None),
 ]
 
-@app.route('/api/players')
-def get_players():
-    player_list = players.get_players()
-    return jsonify(player_list)
+json_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'player_stats.json'))
+
+
+with open(json_file_path) as f:
+    players = json.load(f)
+@app.route("/players")
+def get_all_players():
+    return jsonify(players)
+
+@app.route("/player/<player_name>")
+def get_player(player_name):
+    for player in players:
+        if player["Player"].lower() == player_name.lower():
+            return jsonify(player)
