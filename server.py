ecb725da7258e699fd9f1b5ccc57fe453e7764f4
kayre <kayresantos@gmail.com>
2023-03-16 00:45:19 -0700

    compare players endpoint baby

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
-from flask import Flask, jsonify
-from nba_api.stats.static import players
+from flask import Flask, jsonify, request
 import os
-
+import json
+from playerfunction import player_comparison
 app = Flask(__name__)
 
 origins = [
@@ -9,7 +9,25 @@ origins = [
     os.environ.get("PUBLIC_URL", None),
 ]
 
-@app.route('/api/players')
-def get_players():
-    player_list = players.get_players()
-    return jsonify(player_list)
+json_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'player_stats.json'))
+
+with open(json_file_path) as f:
+    players_json = f.read()
+    players = json.loads(players_json)
+
+@app.route('/players')
+def get_all_players():
+    return jsonify(players)
+
+@app.route('/compare-players')
+def compare_players():
+    # Get player names and stat category from query parameters
+    player1_name = request.args.get('player1_name')
+    player2_name = request.args.get('player2_name')
+    stat_category = request.args.get('stat_category')
+
+    # Call player comparison function with players data from JSON file
+    result = player_comparison(player1_name, player2_name, stat_category)
+
+    # Return result as JSON
+    return jsonify({'result': result})
