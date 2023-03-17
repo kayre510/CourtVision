d6c94018710626137cf17da02edb0a60930bdf62
Brandon Matran <b_matran@comcast.net>
2023-03-17 00:22:17 -0700

    Finished boxscore endpoints

--- server.py
+++ server.py
@@ -1,7 +1,11 @@
 from flask import Flask, jsonify
 import os
 import json
+import requests
+from requests.exceptions import HTTPError
+from todays_boxscore import today_games_dict, game_id_list, endpoint
 
+print(game_id_list)
 app = Flask(__name__)
 
 
@@ -12,6 +16,9 @@ origins = [
 
 json_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'player_stats.json'))
 
+@app.route("/")
+def home():
+    return ("<h1>Welcome to NBA H2H</h1>")
 
 with open(json_file_path) as f:
     players = json.load(f)
@@ -24,3 +31,18 @@ def get_player(player_name):
     for player in players:
         if player["Player"].lower() == player_name.lower():
             return jsonify(player)
+
+#get all of today's boxscores
+@app.route("/boxscores")
+def get_boxscores():
+    return jsonify(today_games_dict)
+
+#get a boxscore
+@app.route("/boxscore/<game_id>")
+def get_boxscore_by_id(game_id=None):
+    url=f"{endpoint}_{game_id}.json"
+    try:
+        response = requests.get(url)
+        return response.json()
+    except HTTPError:
+        return({"Message": "Wrong game id"})
