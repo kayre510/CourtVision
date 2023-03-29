0ccd06ccc6cff35b8697e45ef66fc3181953ff2f
Brandon Matran <b_matran@comcast.net>
2023-03-29 15:09:09 -0700

    updated result modal to fade in

--- src/Components/ResultModal.js
+++ src/Components/ResultModal.js
@@ -1,17 +1,26 @@
 import React from "react";
+import "./ResultModal.css"
 
 export const ResultModal = (result) => {
 
+  const handleCompare = () => {
+    window.location.reload()
+  }
+
+
 
   return (
-    <div>
+    <div className="modalBackground">
+    <div className="modalContainer" animation="fadeInAnimation ease 1s">
       <div className="card" style={{width: "18rem"}}>
         <div className="card-body">
           <p className="card-text">
             {result.result}
           </p>
         </div>
+        <button onClick={handleCompare}>Compare stats again</button>
       </div>
+    </div>
     </div>
   );
 };
